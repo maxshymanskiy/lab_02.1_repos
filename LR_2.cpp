6a3a026c712a7dab8114c342d311615f904de3fc
max-shymanskiy <maxshymanskiy@gmail.com>
2024-09-14 22:34:00 +0300

    first change commit

--- LR_2.cpp
+++ LR_2.cpp
@@ -18,12 +18,12 @@ int main()
 	cout << "m = "; cin >> m;
 	cout << "n = "; cin >> n;
 	
-	z1 = ((m - 1) * sqrt(m) - (n - 1) * sqrt(n)) / (sqrt(pow(m, 3) * n) + (n * m) + pow(m, 2) - m);
-	// z2 = (sqrt(m) - sqrt(n)) / m;
+	//z1 = ((m - 1) * sqrt(m) - (n - 1) * sqrt(n)) / (sqrt(pow(m, 3) * n) + (n * m) + pow(m, 2) - m);
+	 z2 = (sqrt(m) - sqrt(n)) / m;
 	
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
-	// cout << "z2 = " << z2 << endl;
+	//cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 	
 	cin.get();
 	return 0;
